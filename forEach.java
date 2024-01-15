d1e3feb4e075b923872d426f0c426554b0d60136
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-15 13:47:36 +0400

    Update forEach.java

--- forEach.java
+++ forEach.java
@@ -1,8 +1,14 @@
-public class forEach{
-	public static void main(String[] arg){
-		int [] a=new int[] {1,2,3,4,5};
-		for (int i:a){
-			System.out.println(i);
-		}
-	}
+public class forEach {
+  public static void main(String[] arg) {
+    int[] a = new int[] {
+      1,
+      2,
+      3,
+      4,
+      5
+    };
+    for (int i: a) {
+      System.out.println(i);
+    }
+  }
 }
